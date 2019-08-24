8eac123fd164035da1bbf2f54d45fd28d8f75a92
Robert Vincent <robert.vincent@conning.com>
2019-08-24 15:14:02 -0400

    Issue#68 - Turn off the Windows Messenger Customer Experience Improvement Program

--- lib/puppet_x/lsp/security_policy.rb
+++ lib/puppet_x/lsp/security_policy.rb
@@ -907,6 +907,11 @@ class SecurityPolicy
                 :policy_type => 'Registry Values',
                 :reg_type => '4',
             },
+            'Turn off the Windows Messenger Customer Experience Improvement Program' => {
+                :name => 'MACHINE\Software\Policies\Microsoft\Messenger\Client\CEIP',
+                :policy_type => 'Registry Values',
+                :reg_type => '4',
+            },
             'Turn on Windows Defender protection against Potentially Unwanted Applications' => {
                 :name => 'MACHINE\Software\Policies\Microsoft\Windows Defender\MpEngine\MpEnablePus',
                 :policy_type => 'Registry Values',
